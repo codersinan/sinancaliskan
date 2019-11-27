b1d8597eaa55cdd5791a7eccbb7a5841af60c726
Sinan Caliskan <42204545+codersinan@users.noreply.github.com>
2019-11-27 23:22:06 +0300

    lint fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDX5MKFpIGb7L4S2vaA5WzoNtMxg_yTFKY",
-    authDomain: "sinancaliskan-95246.firebaseapp.com",
-    databaseURL: "https://sinancaliskan-95246.firebaseio.com",
-    projectId: "sinancaliskan-95246",
-    storageBucket: "sinancaliskan-95246.appspot.com",
-    messagingSenderId: "450344112216"
+    apiKey: 'AIzaSyDX5MKFpIGb7L4S2vaA5WzoNtMxg_yTFKY',
+    authDomain: 'sinancaliskan-95246.firebaseapp.com',
+    databaseURL: 'https://sinancaliskan-95246.firebaseio.com',
+    projectId: 'sinancaliskan-95246',
+    storageBucket: 'sinancaliskan-95246.appspot.com',
+    messagingSenderId: '450344112216'
   }
 };
 
